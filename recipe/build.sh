7051f72a28b394c23f4419a7f2ed20e44b6622a9
Oleksandr Pavlyk <oleksandr-pavlyk@users.noreply.github.com>
2019-06-05 13:29:15 -0500

    Update/2.1.0 (#4)
    * updated v2.1.0
    
    * Removing numpy from host/run
    
    Also ignore export of mkl-service coming from mkl-devel
    
    * build scripts set MKLROOT=

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,4 @@
 
 # make sure that compiler has been sourced, if necessary
 
-CFLAGS="-I${PREFIX}/include ${CFLAGS}" $PYTHON setup.py build --force install --old-and-unmanageable
+MKLROOT=$PREFIX $PYTHON setup.py build --force install --old-and-unmanageable
